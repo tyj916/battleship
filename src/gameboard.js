b7932f8aee00ba9919d55ce72c210a8b322d2ede
Tan Yi Jun <tanyijun6@gmail.com>
2024-05-14 17:45:44 +0800

    Create gameboard object

---
+++ src/gameboard.js
@@ -0,0 +1,26 @@
+export default function Gameboard(rows = 10, cols = 10) {
+  const gameboard = [];
+
+  for (let i = 0; i < rows; i++) {
+    gameboard.push([]);
+
+    for (let j = 0; j < cols; j++) {
+      gameboard[i].push(null);
+    }
+  }
+
+  function placeShip(ship, coordinate) {
+    const x = coordinate[0];
+    const y = coordinate[1];
+    const shipLength = ship.length;
+
+    for (let i = 0; i < shipLength; i++) {
+      gameboard[x][y + i] = ship;
+    }
+  }
+
+  return {
+    gameboard,
+    placeShip,
+  };
+}
